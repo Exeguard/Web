2648a63704b489e1228192f4460f1d8414ae90ac
Ben McAvoy <ben.mcavoy@tutanota.com>
2023-11-08 00:22:32 +0000

    Update default port

--- main.go
+++ main.go
@@ -10,54 +10,55 @@ import (
 )
 
 const HOT_RELOAD_TEMPLATES = true
+
 var tmpl *template.Template
 
 func main() {
-  var flag = *flag.Int("port", 8080, "host port");
-  var listen = fmt.Sprintf("0.0.0.0:%d", flag);
+	var flag = *flag.Int("port", 8250, "host port")
+	var listen = fmt.Sprintf("0.0.0.0:%d", flag)
 
-  var err error;
-  tmpl, err = template.ParseGlob("templates/*");
+	var err error
+	tmpl, err = template.ParseGlob("templates/*")
 
-  if err != nil {
-    log.Fatal(err);
-  }
+	if err != nil {
+		log.Fatal(err)
+	}
 
-  log.Printf("Hosting at %s", listen);
+	log.Printf("Hosting at %s", listen)
 
-  http.Handle(
-    "/static/",
-    http.StripPrefix(
-      "/static/",
-      http.FileServer(http.Dir("static")),
-    ),
-  );
+	http.Handle(
+		"/static/",
+		http.StripPrefix(
+			"/static/",
+			http.FileServer(http.Dir("static")),
+		),
+	)
 
-  http.HandleFunc("/", handle_request);
+	http.HandleFunc("/", handle_request)
 
-  log.Fatal(http.ListenAndServe(listen, nil));
+	log.Fatal(http.ListenAndServe(listen, nil))
 }
 
 func handle_request(w http.ResponseWriter, r *http.Request) {
-  if HOT_RELOAD_TEMPLATES {
-    var err error;
-    tmpl, err = template.ParseGlob("templates/*");
+	if HOT_RELOAD_TEMPLATES {
+		var err error
+		tmpl, err = template.ParseGlob("templates/*")
 
-    if err != nil {
-      log.Fatal(err);
-    }
-  }
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
 
-  resource := "index.html"
+	resource := "index.html"
 
-  if r.URL.Path != "/" {
-    resource = fmt.Sprintf("%s.html", path.Base(r.URL.Path))
-  }
+	if r.URL.Path != "/" {
+		resource = fmt.Sprintf("%s.html", path.Base(r.URL.Path))
+	}
 
-  log.Printf("Web request for %s", resource);
+	log.Printf("Web request for %s", resource)
 
-  if err := tmpl.ExecuteTemplate(w, resource, nil); err != nil {
-    http.Error(w, err.Error(), http.StatusInternalServerError)
-    fmt.Printf("Error: %s", err)
-  }
+	if err := tmpl.ExecuteTemplate(w, resource, nil); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		fmt.Printf("Error: %s", err)
+	}
 }
