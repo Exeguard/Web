1c0540b958d90cc68fc05691432d86e1b66e1efd
Ben McAvoy <ben.mcavoy@tutanota.com>
2023-11-10 12:23:35 +0000

    Load utility templates into a separate variable

--- main.go
+++ main.go
@@ -6,24 +6,56 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"path"
+	"path/filepath"
+	"strings"
 	"text/template"
 )
 
 const PRODUCTION_BUILD = true
 
+var utilTmpl *template.Template
 var tmpl *template.Template
+
+func load_templates() {
+	var files []string
+	var err error
+
+	err = filepath.Walk("templates", func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		// Check if it's a file and not in the "utils/" subdirectory
+		if !info.IsDir() && !strings.Contains(path, "utils/") {
+			files = append(files, path)
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		log.Fatalf("Reading templates: %s", err)
+	}
+
+	tmpl, err = template.ParseFiles(files...)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	utilTmpl, err = template.ParseGlob("templates/utils/*")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 func main() {
 	var flag = *flag.Int("port", 8250, "host port")
 	var listen = fmt.Sprintf("0.0.0.0:%d", flag)
 
-	var err error
-	tmpl, err = template.ParseGlob("templates/*")
-
-	if err != nil {
-		log.Fatal(err)
-	}
+	load_templates()
 
 	log.Printf("Hosting at %s\n", listen)
 
@@ -41,13 +73,8 @@ func main() {
 }
 
 func handle_request(w http.ResponseWriter, r *http.Request) {
-	if PRODUCTION_BUILD {
-		var err error
-		tmpl, err = template.ParseGlob("templates/*")
-
-		if err != nil {
-			log.Fatal(err)
-		}
+	if !PRODUCTION_BUILD {
+		load_templates()
 	}
 
 	resource := "index.html"
@@ -59,31 +86,30 @@ func handle_request(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Web request for %s\n", resource)
 
 	var header bytes.Buffer
-	tmpl.ExecuteTemplate(&header, "navbar.html", nil)
+	utilTmpl.ExecuteTemplate(&header, "navbar.html", nil)
 
 	var footer bytes.Buffer
-	tmpl.ExecuteTemplate(&footer, "footer.html", nil)
+	utilTmpl.ExecuteTemplate(&footer, "footer.html", nil)
 
 	var head bytes.Buffer
-	tmpl.ExecuteTemplate(&head, "head.html", nil)
+	utilTmpl.ExecuteTemplate(&head, "head.html", nil)
 
 	data := struct {
-	  Navbar string
-    Footer string
-	  Head   string
-    Error  string
-	} {
-	  Navbar: header.String(),
-	  Footer: footer.String(),
-	  Head:   head.String(),
-    Error:  "None",
+		Navbar string
+		Footer string
+		Head   string
+		Error  string
+	}{
+		Navbar: header.String(),
+		Footer: footer.String(),
+		Head:   head.String(),
+		Error:  "None",
 	}
 
 	if err := tmpl.ExecuteTemplate(w, resource, data); err != nil {
-		// http.Error(w, err.Error(), http.StatusInternalServerError)
-    data.Error = err.Error();
+		data.Error = err.Error()
 
-    tmpl.ExecuteTemplate(w, "error.html", data);
+		tmpl.ExecuteTemplate(w, "error.html", data)
 
 		fmt.Printf("Error: %s\n", err)
 	}
