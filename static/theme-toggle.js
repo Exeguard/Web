46051b616719f96e3f92c8eff263a1a2026600ea
Ben McAvoy <ben.mcavoy@tutanota.com>
2023-11-10 11:28:44 +0000

    Reformat theme toggle

--- static/theme-toggle.js
+++ static/theme-toggle.js
@@ -1,19 +1,19 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var prefersDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
+  var prefersDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
 
-    if (prefersDarkMode) {
-        document.body.setAttribute("data-bs-theme", "dark");
-    } else {
-        document.body.setAttribute("data-bs-theme", "light");
-    }
+  if (prefersDarkMode) {
+    document.body.setAttribute("data-bs-theme", "dark");
+  } else {
+    document.body.setAttribute("data-bs-theme", "light");
+  }
 })
 
 document.getElementById("theme-toggle").addEventListener("click", function () {
-    var current = document.body.getAttribute("data-bs-theme");
+  var current = document.body.getAttribute("data-bs-theme");
 
-    if (current === "dark" || current === null) {
-        document.body.setAttribute("data-bs-theme", "light");
-    } else {
-        document.body.setAttribute("data-bs-theme", "dark");
-    }
+  if (current === "dark" || current === null) {
+    document.body.setAttribute("data-bs-theme", "light");
+  } else {
+    document.body.setAttribute("data-bs-theme", "dark");
+  }
 })
